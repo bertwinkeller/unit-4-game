6e983bff9c745af65b3a8e2b397b20a2004066a6
Bert Keller <bertwinkeller@gmail.com>
2019-09-13 23:54:29 -0700

    added comments to read me and html and js files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,99 @@
 
 
 //player objects
+// Contains Name, Health, and Randomized Attack Power
 
 var characters = [{
-    name: 'gandalf',
+    name: 'Gandalf',
     health: 180,
-    attackpower: Math.floor(Math.random() * 25) + 25
+    attackpower: Math.floor(Math.random() * 25) + 15
 },
-{   
-    name: 'gimley',
+{
+    name: 'Gimley',
     health: 150,
-    attackpower: Math.floor(Math.random() * 25) + 30
+    attackpower: Math.floor(Math.random() * 25) + 15
 },
-{   
-    name: 'aragorn',
+{
+    name: 'Aragorn',
     health: 150,
-    attackpower: Math.floor(Math.random() * 25) + 30
+    attackpower: Math.floor(Math.random() * 25) + 15
 },
-{   
-    name: 'legolas',
+{
+    name: 'Legolas',
     health: 200,
-    attackpower: Math.floor(Math.random() * 25) + 20
+    attackpower: Math.floor(Math.random() * 25) + 15
 },
-{   
-    name: 'sauron',
+{
+    name: 'Sauron',
     health: 500,
-    attackpower: Math.floor(Math.random() * 25) + 40
+    attackpower: Math.floor(Math.random() * 25) + 25
     // attackpower: () => Math.floor(Math.random() * 25) + 40
 }];
+
+// Global Variables
 
 let fighter = {};
 let enemy = characters[4];
 let chosenFighterSlot = document.getElementById('chosenFighterColumn');
+let sauronColumn = document.getElementById('sauroncol');
 let isFighterChosen = false;
 
 
 // Loads characters into the fighter Screen
+// Takes Button inputs and appends corresponding image and text to DOM
+// Sets Fighter Variable to correct element in array
 document.addEventListener('click', event => {
-   
-if (event.target.id === 'arButton'){
-    isFighterChosen = true;
-    fighter = characters[2];
-    chosenFighterSlot.innerHTML = '';
-    chosenFighterSlot.innerHTML += '<img src="./assets/images/aragorn.jpg" id="fighterImage">';
-    chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 150</h4>';
-}else if(event.target.id === 'legButton'){
-    isFighterChosen = true;
-    fighter = characters[3];
-    chosenFighterSlot.innerHTML = '';
-    chosenFighterSlot.innerHTML += '<img src="./assets/images/legolas.png" id="fighterImage">';
-    chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 200</h4>';
-}else if(event.target.id === 'gimButton'){
-    isFighterChosen = true;
-    fighter = characters[1];
-    chosenFighterSlot.innerHTML = '';
-    chosenFighterSlot.innerHTML += '<img src="./assets/images/gimley.jpg" id="fighterImage">';
-    chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 150</h4>';
-}else if(event.target.id === 'ganButton'){
-    isFighterChosen = true;
-    fighter = characters[0];
-    chosenFighterSlot.innerHTML = '';
-    chosenFighterSlot.innerHTML += '<img src="./assets/images/gandalf_new.jpg" id="fighterImage">';
-    chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 180</h4>';
-}})
 
+    if (event.target.id === 'arButton') {
+        isFighterChosen = true;
+        fighter = characters[2];
+        chosenFighterSlot.innerHTML = '';
+        chosenFighterSlot.innerHTML += '<img src="./assets/images/aragorn.jpg" id="fighterImage">';
+        chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 150</h4>';
+    } else if (event.target.id === 'legButton') {
+        isFighterChosen = true;
+        fighter = characters[3];
+        chosenFighterSlot.innerHTML = '';
+        chosenFighterSlot.innerHTML += '<img src="./assets/images/legolas.png" id="fighterImage">';
+        chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 200</h4>';
+    } else if (event.target.id === 'gimButton') {
+        isFighterChosen = true;
+        fighter = characters[1];
+        chosenFighterSlot.innerHTML = '';
+        chosenFighterSlot.innerHTML += '<img src="./assets/images/gimley.jpg" id="fighterImage">';
+        chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 150</h4>';
+    } else if (event.target.id === 'ganButton') {
+        isFighterChosen = true;
+        fighter = characters[0];
+        chosenFighterSlot.innerHTML = '';
+        chosenFighterSlot.innerHTML += '<img src="./assets/images/gandalf_new.jpg" id="fighterImage">';
+        chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 180</h4>';
+    }
+})
 
+//function that checks to see if hero wins or Sauron wins
+//asks for new character when one is defeated
+//clears column depending on winner and loser
+const newHero = function () {
+    if(fighter.health <= 0) {
+        let deadHeroMessage = `${fighter.name} was killed by Sauron! Choose another fighter to destroy him and bring peace to Middle Earth!`;
+        alert(deadHeroMessage);
+        chosenFighterSlot.innerHTML = '';
+    }else if(enemy.health <= 0 || fighter.health <= 0) {
+        let deadEnemyMessage = `${fighter.name} killed Sauron and brought peace to Middle Earth! Refresh the Page to play again!`;
+        alert(deadEnemyMessage);
+        sauronColumn.innerHTML = '';
+
+    }
+};
+
+//attack button functionality
+//updates dom with hit and damage information for both users
+// calls function to select a new hero to fight sauron
 document.getElementById('attackButton').addEventListener('click', event => {
     var newText = document.getElementById('statRow');
     var newHealth = document.getElementById('updateHealth');
-    var sauronNewHealth = document.getElementById('sauronHealth')
+    var sauronNewHealth = document.getElementById('sauronHealth');
     newText.innerHTML = '';
     newHealth.innerHTML = '';
     sauronNewHealth.innerHTML = '';
@@ -78,14 +103,8 @@ document.getElementById('attackButton').addEventListener('click', event => {
     enemy.health -= (fighterHitStrength);
     newHealth.innerHTML += 'Health Points: ' + fighter.health;
     sauronNewHealth.innerHTML += 'Health Points: ' + enemy.health;
-    newText.innerHTML += 'You hit Sauron for ' + fighterHitStrength + ', '
-    
-    newText.innerHTML += 'Sauron Hit you for ' + enemyHitStrength;
+    newText.innerHTML += `${fighter.name} hit Sauron for ` + fighterHitStrength + ' dmg , ';
+    newText.innerHTML += `Sauron Hit ${fighter.name} for ` + enemyHitStrength + ' dmg';
+    newHero();
 
-    
 })
-
-const newHero = function() {
-when
-
-}
