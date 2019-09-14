20138a8d64cca5c6cc27bb50ea1c13f6b4d438db
Bert Keller <bertwinkeller@gmail.com>
2019-09-13 22:01:24 -0700

    attack functionality working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-// global variables
-var playerChosen = false;
 
 
 //player objects
@@ -7,76 +5,87 @@ var playerChosen = false;
 var characters = [{
     name: 'gandalf',
     health: 180,
-    attackpower: () => Math.floor(Math.random() * 25) + 25
+    attackpower: Math.floor(Math.random() * 25) + 25
 },
 {   
     name: 'gimley',
     health: 150,
-    attackpower: () => Math.floor(Math.random() * 25) + 30
+    attackpower: Math.floor(Math.random() * 25) + 30
 },
 {   
     name: 'aragorn',
     health: 150,
-    attackpower: () => Math.floor(Math.random() * 25) + 30
+    attackpower: Math.floor(Math.random() * 25) + 30
 },
 {   
     name: 'legolas',
     health: 200,
-    attackpower: () => Math.floor(Math.random() * 25) + 20
+    attackpower: Math.floor(Math.random() * 25) + 20
 },
 {   
     name: 'sauron',
     health: 500,
-    attackpower: () => Math.floor(Math.random() * 25) + 40
+    attackpower: Math.floor(Math.random() * 25) + 40
+    // attackpower: () => Math.floor(Math.random() * 25) + 40
 }];
 
 let fighter = {};
 let enemy = characters[4];
 let chosenFighterSlot = document.getElementById('chosenFighterColumn');
-// let isFighterChosen = False;
+let isFighterChosen = false;
 
 
 // Loads characters into the fighter Screen
 document.addEventListener('click', event => {
    
 if (event.target.id === 'arButton'){
-    // isFighterChosen = True;
+    isFighterChosen = true;
     fighter = characters[2];
     chosenFighterSlot.innerHTML = '';
     chosenFighterSlot.innerHTML += '<img src="./assets/images/aragorn.jpg" id="fighterImage">';
-    chosenFighterSlot.innerHTML += '<h4>Health Points: 150</h4>';
+    chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 150</h4>';
 }else if(event.target.id === 'legButton'){
-    // isFighterChosen = True;
+    isFighterChosen = true;
     fighter = characters[3];
     chosenFighterSlot.innerHTML = '';
     chosenFighterSlot.innerHTML += '<img src="./assets/images/legolas.png" id="fighterImage">';
-    chosenFighterSlot.innerHTML += '<h4>Health Points: 200</h4>';
+    chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 200</h4>';
 }else if(event.target.id === 'gimButton'){
-    // isFighterChosen = True;
+    isFighterChosen = true;
     fighter = characters[1];
     chosenFighterSlot.innerHTML = '';
     chosenFighterSlot.innerHTML += '<img src="./assets/images/gimley.jpg" id="fighterImage">';
-    chosenFighterSlot.innerHTML += '<h4>Health Points: 150</h4>';
+    chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 150</h4>';
 }else if(event.target.id === 'ganButton'){
-    // isFighterChosen = True;
+    isFighterChosen = true;
     fighter = characters[0];
     chosenFighterSlot.innerHTML = '';
     chosenFighterSlot.innerHTML += '<img src="./assets/images/gandalf_new.jpg" id="fighterImage">';
-    chosenFighterSlot.innerHTML += '<h4>Health Points: 180</h4>';
+    chosenFighterSlot.innerHTML += '<h4 id="updateHealth">Health Points: 180</h4>';
 }})
-
-const printInfo = () =>{
-    
-    
-}
 
 
 document.getElementById('attackButton').addEventListener('click', event => {
-    fighter.health -= enemy.attackpower();
-    enemy.health -= fighter.attackpower();
-    // fighter -= characters[characters.length - 1].attackpower()
-    // characters[characters.length - 1] -= fighter.attackpower()
-    console.log(fighter.health);
-    console.log(enemy.health);
+    var newText = document.getElementById('statRow');
+    var newHealth = document.getElementById('updateHealth');
+    var sauronNewHealth = document.getElementById('sauronHealth')
+    newText.innerHTML = '';
+    newHealth.innerHTML = '';
+    sauronNewHealth.innerHTML = '';
+    let enemyHitStrength = enemy.attackpower;
+    let fighterHitStrength = fighter.attackpower;
+    fighter.health -= enemyHitStrength;
+    enemy.health -= (fighterHitStrength);
+    newHealth.innerHTML += 'Health Points: ' + fighter.health;
+    sauronNewHealth.innerHTML += 'Health Points: ' + enemy.health;
+    newText.innerHTML += 'You hit Sauron for ' + fighterHitStrength + ', '
+    
+    newText.innerHTML += 'Sauron Hit you for ' + enemyHitStrength;
+
     
 })
+
+const newHero = function() {
+when
+
+}
